006ea2bd11f932b8451b79f2f09dc737b7211ee0
Lorenzo Migani <lorenzo.migani@flain.co>
2024-11-03 11:54:05 +0100

    feat: background

--- Colors/ContentView.swift
+++ Colors/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+
+    @State private var background: MeshGradient = MeshGradient(width: 2, height: 2, points: [
+        [0, 0], [1, 0],
+        [0, 1], [1, 1]
+    ], colors: [
+        .pink, .lilla,
+        .lightBlue, .purple
+    ])
+    
+    @State private var backgroundDark: MeshGradient = MeshGradient(width: 2, height: 2, points: [
+        [0, 0], [1, 0],
+        [0, 1], [1, 1]
+    ], colors: [
+        .ivory, .zenith,
+        .blue, .ivory
+    ])
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack{
+                Group{
+                    if colorScheme == .light{
+                        background
+                    } else {
+                        backgroundDark
+                    }
+                }
+                .ignoresSafeArea()
+            }
         }
-        .padding()
     }
 }
 
